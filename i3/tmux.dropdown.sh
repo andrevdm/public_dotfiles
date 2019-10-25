e5a157e90324827d437105a517142c003d5d0bf8
Andre Van Der Merwe <andre@andrevdm.com>
2019-10-25 18:46:14 +0200

    cleanup

--- i3/tmux.dropdown.sh
+++ i3/tmux.dropdown.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 tmux has-session -t dropdown
 if [ $? != 0 ]
 then
@@ -13,9 +13,6 @@ then
 
     tmux new-window -n "org" -t dropdown
     tmux send-keys -t dropdown:2.0 'cd ~/Dropbox/org; nvim ~/Dropbox/org/hyrax.org' C-m
-
-    tmux new-window -n "cmus" -t dropdown
-    tmux send-keys -t dropdown:3.0 'cmus' C-m
 
     tmux select-window -t dropdown:1
     tmux select-pane -t dropdown:1.1
